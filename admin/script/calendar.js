a65f5b756518d35b0b7e81b0c5ac6c8ef06cd651
rui0421 <rui.04215@icloud.com>
2024-11-25 11:03:05 +0900

    変更

--- admin/script/calendar.js
+++ admin/script/calendar.js
@@ -1,43 +1,97 @@
-'use strict'
+// 曜日の定義
+const week = ["日", "月", "火", "水", "木", "金", "土"];
 
-const date = new Date();
-const year = date.getFullYear();
-const month = date.getMonth() + 1;
-const firstDate = new Date(year,month - 1, 1);
-const firstDay = firstDate.getDay();
-const lastDate = new Date(year,month,0);
-const lastDayCount = lastDate.getDate();
+// 今日の日付
+var today = new Date();
 
-let dayCount = 1;
-let createHtml = '';
+// 表示用の日付
+var showDate = new Date(today.getFullYear(), today.getMonth(), 1);
 
-createHtml = '<h1>' + year + '/' + month + '</h1>'
-createHtml += '<table>' + '<tr>';
+// 表示された時
+window.onload = function () {
+  // カレンダーの表示（引数には表示用の日付を設定）
+  showCalendar(showDate);
+};
 
-const weeks = ['日','月','火','水','木','金','土'];
-for(let i = 0; i < weeks.length; i++){
-    createHtml += '<td>' + weeks[i] + '</td>';
+/**
+ * カレンダーの表示
+ */
+function showCalendar(date) {
+  // 年
+  var year = date.getFullYear();
+  // 月
+  var month = date.getMonth() + 1;
+  
+  // ヘッダーの年月に表示する文字列
+  var showDateStr = year + "年 " + month + "月";
+  
+  // ヘッダーの年月部分に埋め込み
+  document.querySelector('#year_month_label').innerHTML = showDateStr;
+  
+  // カレンダーテーブルを作成する（HTMLが返却される）
+  var calendarTable = createCalendarTable(year, month);
+  
+  // カレンダー表示部分に埋め込み
+  document.querySelector('#calendar_body').innerHTML = calendarTable;
 }
-createHtml += '</tr>';
 
-for(let n = 0; n < 6; n++){
-    createHtml += '<tr>';
-
-        for(let d = 0; d < 7; d++){
-            if(n == 0 && d < firstDay){
-                createHtml += '<td></td>'
-            }else if(dayCount > lastDayCount) {
-                createHtml += '<td></td>'
-            }else { 
-                createHtml += '<td>' + dayCount + '</td>';
-                dayCount++
-            }
-        }
-
-        createHtml += '</tr>'
+/**
+ * カレンダーテーブルの作成
+ */
+function createCalendarTable(year, month) {
+  // HTML用の変数
+  var _html = '';
+  
+  // tableタグ
+  _html += '<table class="calendar_tbl">';
+  
+  // テーブルのヘッダー（曜日）
+  _html += '<tr>';
+  for (var i = 0; i < week.length; i++) {
+    _html += "<th>" + week[i] + "</th>";
+  }
+  _html += '</tr>';
+  
+  // ---------------------
+  
+  // 表示するカレンダー年月の1日の曜日を取得
+  var startDayOfWeek = new Date(year, month - 1, 1).getDay();
+  
+  // 日付
+  var countDay = 0;
+  
+  // 月の末日
+  var monthOfEndDay = new Date(year, month, 0).getDate()
+  
+  // 6行分繰り返し
+  for (var i = 0; i < 6; i++) {
+    _html += '<tr>';
+    
+    // 7列（曜日の数）分繰り返し
+    for (var j = 0; j < week.length; j++) {
+      // １行目で開始曜日と同じ場合
+      if (i == 0 && j == startDayOfWeek) {
+        // 日付+1
+        countDay++;
+        // tdタグ（日付有りが分かるようにクラス属性に"with_date"を設定）
+        _html += '<td class="with_date">' + countDay + '</td>';
+      }
+      // 日付が0以外で、日付が末日より小さい場合
+      else if (countDay != 0 && countDay < monthOfEndDay) {
+        // 日付+1
+        countDay++;
+        // tdタグ（日付有りが分かるようにクラス属性に"with_date"を設定）
+        _html += '<td class="with_date">' + countDay + '</td>';
+      }
+      else {
+        // tdタグ（日付無しが分かるようにクラス属性に"no_date"を設定）
+        _html += '<td class="no_date"></td>';
+      }
+    }
+    _html += '</tr>';
+  }
+  _html += '</table>';
+  
+  // 組み立てたHTMLを返却
+  return _html;
 }
-createHtml += '</table>'
-
-document.querySelector('#calendar').innerHTML = createHtml;
-
-console.log(createHtml);
